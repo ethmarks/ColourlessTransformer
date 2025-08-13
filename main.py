79f3d73f5e95e1193cae7d2c9b51e6f1ec69a863
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2025-08-13 17:20:45 -0400

    rename start_streamlit.py to main.py

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-def main():
-    print("Hello from colourlesstransformer!")
-
+import subprocess
+import sys
 
 if __name__ == "__main__":
-    main()
+    try:
+        subprocess.run([sys.executable, "-m", "streamlit", "run", "app.py"], check=True)
+    except subprocess.CalledProcessError as e:
+        print(f"Error running Streamlit: {e}")
