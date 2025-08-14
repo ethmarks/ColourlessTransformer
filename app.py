611ded09ac75bedb231b7402122174c61de5c9e4
Ethan Marks <87040432+ColourlessSpearmint@users.noreply.github.com>
2025-08-13 20:11:18 -0400

    move app text from streamlit_description.md to app.py

--- app.py
+++ app.py
@@ -8,16 +8,33 @@ from torch import OutOfMemoryError
 # Set page config to wide mode
 st.set_page_config(layout="wide")
 
+markdown_content = """
+# Colourless Transformer - Streamlit
 
-# Function to read markdown from a file
-def load_markdown(file_path):
-    with open(file_path, "r") as file:
-        return file.read()
+Created by **Ethan Marks** ([@ColourlessSpearmint](https://github.com/ColourlessSpearmint)).
 
+**ColourlessTransformer** is an interface for the Paint Transformer neural network, which performs feed-forward neural painting with stroke prediction.
 
-# Load markdown description from a file
-markdown_path = "streamlit_description.md"
-markdown_content = load_markdown(markdown_path)
+## Usage
+
+### **Upload an Image**
+
+Use the file uploader to drag and drop an image or select it from your file system. Supported formats: .png, .jpg, .jpeg.
+
+The app will automatically resize the image to a maximum of 512 pixels. This will drastically decrease processing time for large images without significantly affecting output quality.
+
+### **Choose Output Format**
+
+Animation Toggle: Enable the checkbox to generate an animation (GIF) instead of a static image.
+
+### **Generate Results**
+
+Click the Generate button to process your uploaded image. Depending on your hardware, the processing should take between a few seconds and a few minutes.
+
+### **View Results**
+
+Once processing is complete, view the result in the right column. You can download the result by right-clicking and selecting "Save Image As..."
+"""
 st.markdown(markdown_content)
 
 # Initialize session state variables if not already set
